b0372160a6091f8adfad0fb7c7a2337fde215d50
moshfiqur-code-guru <mdsadik3342@gmail.com>
2026-07-08 19:12:17 +0600

    animation

--- day003/assets/js/script.js
+++ day003/assets/js/script.js
@@ -5,3 +5,9 @@ const header = document.getElementById("header");
 window.addEventListener("scroll", () => {
     header.classList.toggle("scrolled", window.scrollY > 300);
 });
+
+window.addEventListener("DOMContentLoaded", addAClass);
+
+function addAClass() {
+    document.body.classList.add("animation-start")
+}
