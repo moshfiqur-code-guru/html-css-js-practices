8e6118a37d9009eb60b46f99d1fcb7a1aceca83f
moshfiqur-code-guru <mdsadik3342@gmail.com>
2026-07-11 12:54:02 +0600

    new commit

--- day003/assets/js/script.js
+++ day003/assets/js/script.js
@@ -11,3 +11,17 @@ window.addEventListener("DOMContentLoaded", addAClass);
 function addAClass() {
     document.body.classList.add("animation-start")
 }
+
+
+$(document).ready(function () {
+
+   $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    stagePadding: 100,
+    nav: true,
+    dots: true,
+    items: 1
+});
+
+});
